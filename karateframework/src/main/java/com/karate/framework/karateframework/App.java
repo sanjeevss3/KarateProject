a40e50bf7170bef340b7c332709894ccc3f9e21b
sanjeevss3 <sanjeevss.chaudhary@gmail.com>
2024-09-01 23:36:47 +0530

    Karate framework

---
+++ karateframework/src/main/java/com/karate/framework/karateframework/App.java
@@ -0,0 +1,13 @@
+package com.karate.framework.karateframework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
